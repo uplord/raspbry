482511899cd10d622b4effb2164c78678f5e6204
Michael <michael@uplord.co.uk>
2024-08-15 22:23:54 +0100

    section heading

--- src/components/Block/Section/index.vue
+++ src/components/Block/Section/index.vue
@@ -7,7 +7,7 @@
       <div class="inner-container">
         <h2>We believe in the power of quality, direct and honest communication.</h2>
         <p><a href="">Learn more about our studio</a></p>
-        <h3>Small design team from Dundee, Scotland.</h3>
+        <h4>Small design team from Dundee, Scotland.</h4>
         <p>We’re small by design – allowing us to effectively communicate with your team. By fostering an environment of clear dialogue, we’re able to fully understand your unique vision and how to achieve your project goals.</p>
         <p>Not only does this make the process smoother – it ensures the best end product for you and your audience.</p>
       </div>
